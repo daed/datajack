e8b8d1258570ae80aebae6f6c46b3955fc35119c
Brad Arnett <brad.arnett@gmail.com>
2021-09-13 01:04:44 -0700

    fix mobile stuff

--- rules/src/app/page/page.component.ts
+++ rules/src/app/page/page.component.ts
@@ -16,4 +16,26 @@ export class PageComponent implements OnInit {
 
   }
 
+  exitNavbar(e: any): void {
+    console.log(e.target.parentElement.id);
+    if(e.target.parentElement.id == 'yourID') {
+      var d = document.getElementById('sidebar-wrapper');
+      d?.classList.toggle("toggle-sidebar");
+    }
+  }
+
+  toggleNavbar(): void {
+    var d = document.getElementById('sidebar-wrapper');
+    d?.classList.toggle("toggle-sidebar");
+    d = document.getElementById('toggle-sidebar-label');
+    console.log(d);
+    if (d?.innerText.includes("Show")) {
+      console.log("setting show to hide");
+      d.innerText = d?.innerText.replace("Show", "Hide");
+    }
+    else if (d?.innerText.includes("Hide")) {
+      d.innerText = d?.innerText.replace("Hide", "Show");
+    }
+  }
+
 }
